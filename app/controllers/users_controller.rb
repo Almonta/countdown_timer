30b2d63ad990ce5b9f91693e1925bb5cef1ca32d
motohiro takagi <tkg.8.mthr@gmail.com>
2021-09-22 20:57:16 +0900

    userテーブルにemailカラム追加 #5

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:name, :birthday, :password, :password_confirmation)
+    params.require(:user).permit(:name, :birthday, :email, :password, :password_confirmation)
   end
 end
