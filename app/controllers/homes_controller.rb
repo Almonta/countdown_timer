837a0c02612596a1515117175188255358738d0e
motohiro takagi <tkg.8.mthr@gmail.com>
2021-09-26 13:54:36 +0900

    フレーズをランダム表示できるようにした #7

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -4,10 +4,10 @@ class HomesController < ApplicationController
     @targets = Target.where(user_id: current_user.id)
     
     # Phraseのidを全件取得
-
+    # phrase_ids = Phrase.select('id')
     # 取得したidを配列に入れる
 
     # 配列に入れたidの中からランダムにidを取り出す
-    @phrase = Phrase.find(1)
+    @phrase = Phrase.find(rand(Phrase.first.id..Phrase.last.id))
   end
 end
